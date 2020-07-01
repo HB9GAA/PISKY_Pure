14e5444597938da472a02075696808221d5db3ca
HB9GAA <hb9gaa@arrl.net>
2020-07-01 14:03:50 +0200

    minor adaptations

--- tracker/relais.c
+++ tracker/relais.c
@@ -2,8 +2,23 @@
 
 uint8_t Relais[4] = {1, 4, 26, 29};			//Initial pin numbers of relays 0 to 3
 int temp = 0;
-int RelaisStatusTemp[4] = {0, 0, 0, 0};	
+int RelaisStatusTemp[4] = {1, 1, 1, 1};	
 	
+
+void RelaisStatus(struct TGPS *GPS)
+	{
+	printf("\nRelais-Status at GPS-Alt = %5dm and Vspeed = %5.2fm/s\n", GPS->Altitude, GPS->AscentRate);		
+	printf("|------|------|--------|--------|--------|--------|\n");	
+	printf("|Relais|Status|  Asc-ON| Asc-OFF|  Des-ON| Des-OFF|\n");	
+	for (int Rel = 0; Rel < 4; Rel++)
+		{
+		printf("|------|------|--------|--------|--------|--------|\n");		
+		printf("|  %d   |  %d   |  %5dm|  %5dm|  %5dm|  %5dm|\n", Rel, RelaisStatusTemp[Rel], Config.RelaisConfig[Rel].AscendON, Config.RelaisConfig[Rel].AscendOFF, Config.RelaisConfig[Rel].DescendON, Config.RelaisConfig[Rel].DescendOFF);
+		}
+	printf("|------|------|--------|--------|--------|--------|\n");	
+	}	
+
+
 void *RelaisLoop(void *some_void_ptr)
 	{
 	struct TGPS *GPS;
@@ -12,52 +27,34 @@ void *RelaisLoop(void *some_void_ptr)
 	for (int i = 0; i < 4; i++)
 		{
 		Config.RelaisConfig[i].RelaisPin = Relais[i];
+		pinMode (Config.RelaisConfig[1].RelaisPin, OUTPUT);
 		}
 
-
-	// We have 2 LED outputs
-	pinMode (Config.RelaisConfig[0].RelaisPin, OUTPUT);
-	pinMode (Config.RelaisConfig[1].RelaisPin, OUTPUT);
-	pinMode (Config.RelaisConfig[2].RelaisPin, OUTPUT);
-	pinMode (Config.RelaisConfig[3].RelaisPin, OUTPUT);
-	
-	
-	for (int Rel = 0; Rel < 4; Rel++)
-		{
-		printf("Relais %d settings\n", Rel);
-		printf("..    - ASC_ON = %5d m,\tASC_OFF = %5d m\n", Config.RelaisConfig[Rel].AscendON, Config.RelaisConfig[Rel].AscendOFF);
-		printf("..    - DES_ON = %5d m,\tDES_OFF = %5d m\n", Config.RelaisConfig[Rel].DescendON, Config.RelaisConfig[Rel].DescendOFF);
-		}
-	
+	RelaisStatus(GPS);
+		
 	while (1)
 		{
-		if(GPS->AscentRate > 0.1)
-			{
-			for (int i = 0; i < 4; i++)
-				{		
-				if((GPS->Altitude > Config.RelaisConfig[i].AscendON) && (GPS->Altitude < Config.RelaisConfig[i].AscendOFF))	
-					digitalWrite (Config.RelaisConfig[i].RelaisPin, 1);
+		for (int i = 0; i < 4; i++)
+			{			
+			if(GPS->AscentRate > 0.1)
+				{
+				if((GPS->Altitude > Config.RelaisConfig[i].AscendON) && (GPS->Altitude < Config.RelaisConfig[i].AscendOFF))
+					RelaisStatusTemp[i] = 1;
 				else
-					digitalWrite (Config.RelaisConfig[i].RelaisPin, 0);
+					RelaisStatusTemp[i] = 0;
 				}
-			}
-		else if(GPS->AscentRate < 0.1)
-			{
-			for (int i = 0; i < 4; i++)
-				{		
-				if((GPS->Altitude < Config.RelaisConfig[i].AscendON) && (GPS->Altitude > Config.RelaisConfig[i].AscendOFF))	
-					digitalWrite (Config.RelaisConfig[i].RelaisPin, 1);
+			else if(GPS->AscentRate < -0.1)
+				{
+				if((GPS->Altitude < Config.RelaisConfig[i].DescendON) && (GPS->Altitude > Config.RelaisConfig[i].DescendOFF))	
+					RelaisStatusTemp[i] = 1;
 				else
-					digitalWrite (Config.RelaisConfig[i].RelaisPin, 0);
+					RelaisStatusTemp[i] = 0;
 				}
+			digitalWrite(Config.RelaisConfig[i].RelaisPin, RelaisStatusTemp[i]);
 			}
-		
-
-				printf("ASC_Rate = %5.1f m/s,\tASC_ON = %5d m,\tGPS-Alt = %5d m\n", GPS->AscentRate, Config.RelaisConfig[0].AscendON, GPS->Altitude);
 		temp++;
-		
-		usleep(3000000);
-		
+		RelaisStatus(GPS);		
+		usleep(10000000);
 		}
 
 	return 0;
